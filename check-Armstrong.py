0d2fb8a8ca29bd82b544f03504e3f0a11728277a
samuelmuabia <samuel.planet1@gmail.com>
2023-01-29 11:26:13 +0600

    Check Prime solution in better and optimized way

--- check-Armstrong.py
+++ check-Armstrong.py
@@ -28,4 +28,10 @@ for digit in numberToString:
 if sumOfCube == number:
     print(f"Yes,The number {number} is an Armstrong Number")
 else:
-    print(f"No,The number {number} is not an Armstrong Number") 
+    print(f"No,The number {number} is not an Armstrong Number")
+
+# Both solutions have the same time complexity of O(n), where n is the number of digits in the input number, because both solutions iterate through each digit of the input number once.
+
+# Both solutions also have the same space complexity of O(1), because both solutions use only a constant amount of additional memory.
+
+# The reason the second solution is more efficient than the first one is because it avoids the overhead of using the modulus and integer division operations. In the first solution, you are using modulus operator to get the rightmost digit and then you are dividing the number by 10 to remove the rightmost digit. These operations can be computationally expensive and time-consuming, especially for large input numbers. 
--- check-prime.py
+++ check-prime.py
@@ -2,6 +2,8 @@
 
 def checkPrime(number):
     isPrime= True
+    if number<2:
+        isPrime = False
     for i in range(2,number//2):
         if number%i==0:
             isPrime = False
@@ -15,4 +17,26 @@ number = 13
 if(checkPrime(number)):
     print(f"The number {number} is a Prime Number")
 else:
-    print(f"The number {number} is not a Prime Number")     
+    print(f"The number {number} is not a Prime Number")
+
+# The time complexity of this solution is O(n/2) where n is the input number. Because for each number in the range of 2 to number//2 you are checking if the input number is divisible by that number.
+
+# The space complexity of this solution is O(1) because you are only using a single variable isPrime to keep track of whether the input number is prime or not.
+
+number = 13
+
+isPrime = True
+if number <2 :
+    isPrime=False
+else:
+    for i in range (2,int(number**(0.5))+1):
+        if number%i==0:
+            isPrime==False
+            break
+if isPrime :
+    print(f"The number {number} is a Prime Number")
+else:
+    print(f"The number {number} is not a Prime Number")
+# Here, the time complexity is O(sqrt(n)) as well.
+# The space complexity is still O(1).
+# better solution
