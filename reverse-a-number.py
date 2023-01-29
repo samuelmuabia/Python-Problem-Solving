0300803c2715f517832b617e1a85532b5d68f830
samuelmuabia <samuel.planet1@gmail.com>
2023-01-29 10:40:47 +0600

    reverse of a integer solved in more two ways

--- reverse-a-number.py
+++ reverse-a-number.py
@@ -8,7 +8,23 @@ def reverseAInteger(number):
             reversedNumber = remainder + reversedNumber*10
     return reversedNumber
 
-
-
 reversedNumber=reverseAInteger(326456425)
 print(reversedNumber)
+
+#problem has time complexity of O(n) and space complexity of O(1)
+
+# recursive method with time complexity of O(n) and space complexity of O(n) so not recommended
+# def reverse_int(x):
+#     if x < 10:
+#         return x
+#     else:
+#         return int(str(x % 10) + str(reverse_int(x // 10)))
+
+# print(reverse_int(123))
+
+# simple way to solve this
+
+# number = 123123
+# numberToString= str(number)
+# numberToString= numberToString[::-1]
+# print(int(numberToString))
