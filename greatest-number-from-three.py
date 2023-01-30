98a0e62695ded9d233e140f76136284bb898c3cd
samuelmuabia <samuel.planet1@gmail.com>
2023-01-30 09:24:34 +0600

    Greatest Number

---
+++ greatest-number-from-three.py
@@ -0,0 +1,10 @@
+number1= 15
+number2= 20
+number3= 11
+if number1>=number2>=number3:
+    print(f"{number1} is greatest from all three")
+elif number1<=number2>=number3:
+    print(f"{number2} is greatest from all three")
+elif number1<=number2<=number3:
+    print(f"{number3} is greatest from all three")
+
