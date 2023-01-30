c16ba5ea99bd22c17dd46c995ef302b946e11617
samuelmuabia <samuel.planet1@gmail.com>
2023-01-30 09:13:39 +0600

    Palindrome Check in recursive method added

--- palindrome-check.py
+++ palindrome-check.py
@@ -25,8 +25,29 @@ else :
     print(f"The Number {number} is not a palindrome")
 
 # another way to reverse
+
 numberToString = numberToString[::-1]
 if int(newNumber)==number:
+    print(f"The Number {number} is a palindrome")
+else :
+    print(f"The Number {number} is not a palindrome")
+
+
+# Using Recursive Method
+def reverse(number):
+    if number<10:
+        return number
+    else:
+        return int(str(number%10) + str(reverse(number//10)))
+
+def isPalindrome(number):
+    reversedNumber = reverse(number)
+    if reversedNumber==number:
+        return True
+    else:
+        return False
+number = 121
+if isPalindrome(number):
     print(f"The Number {number} is a palindrome")
 else :
     print(f"The Number {number} is not a palindrome")
