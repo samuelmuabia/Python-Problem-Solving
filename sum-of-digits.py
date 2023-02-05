453b052499a8bd641bfdcb0f30fbb59eeab9e3df
samuelmuabia <samuel.planet1@gmail.com>
2023-02-05 13:17:04 +0600

    summation of digits using loop

--- sum-of-digits.py
+++ sum-of-digits.py
@@ -1,8 +1,19 @@
 # Write a program in Python to find sum of digits of a number using recursion?
 
+
+# using recursion
 def sumOfDigits(number):
     if number==0:
         return 0
     else:
         return number%10+sumOfDigits(number//10)
 print(sumOfDigits(12345))
+
+#using loop
+
+number = 12345
+sum = 0
+while number>0:
+    sum = sum + number%10
+    number = number//10
+print (sum) 
